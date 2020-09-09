0794cb706bd7c6663c77b048d69481585832ecce
Flcable <flcable@gmail.com>
2020-09-10 00:16:47 +0100

    Updates

---
+++ precache-manifest.9df174e80ca019e7c62f6eaf2f953256.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5fe15088568cec59d77c6f8837d9ca",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "d0228f6b284c64a284fe",
+    "url": "/myportfoliosite/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "6aace416be94f46a7ec7",
+    "url": "/myportfoliosite/static/css/main.b50a439e.chunk.css"
+  },
+  {
+    "revision": "d0228f6b284c64a284fe",
+    "url": "/myportfoliosite/static/js/2.dbdc42af.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfoliosite/static/js/2.dbdc42af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aace416be94f46a7ec7",
+    "url": "/myportfoliosite/static/js/main.6678f5f5.chunk.js"
+  },
+  {
+    "revision": "a9c8716d10921dbee87d",
+    "url": "/myportfoliosite/static/js/runtime-main.5aa7204c.js"
+  },
+  {
+    "revision": "34e5cf8ee251f47ee281a9df670f1545",
+    "url": "/myportfoliosite/static/media/avatar.34e5cf8e.png"
+  },
+  {
+    "revision": "a510bbc64ed2ddbefe1a4e7745e8805c",
+    "url": "/myportfoliosite/static/media/avatar_.a510bbc6.png"
+  }
+]);
