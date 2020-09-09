39e659bf982186760a7c61ad2505a88ba2c3d31e
Flcable <flcable@gmail.com>
2020-09-09 18:09:55 +0100

    Designing contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import avatar from "../img/avatar.png"
 
 class Contact extends Component {
     render() {
-        return(
-            <div><h1>Contact Page</h1></div>
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Flavio Cruz</h2>
+                        <img
+                            alt="avatar" src={avatar}
+                            style={{ height: '250px' }}
+                        />
+
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Nam hendrerit nisi sed sollicitudin pellentesque.
+                            Nunc posuere purus rhoncus pulvinar aliquam.
+                            Ut aliquet tristique nisl vitae volutpat.
+                            Nulla aliquet porttitor venenatis.
+                            Donec a dui et dui fringilla consectetur id nec massa.
+                            Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque
+                        </p>
+
+                    </Cell>
+
+                    <Cell col={6}>
+                        
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                    <div className="contact-list">
+                       
+
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                                    (351) 936-458-996
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-envelope" aria-hidden="true"/>
+                                    flcable@mail.com 
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-skype" aria-hidden="true"/>
+                                    Flcable1
+                                </ListItemContent>
+                            </ListItem>
+                            
+                        </List>
+                    </div>    
+                       
+                    </Cell>
+                </Grid>
+            </div>
+
         )
     }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl';
 
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0};
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab === 0) {
+            return(
+                /* Peoject 1 - For add more project card, just copy and paste all content of the card tag and make the changes.*/
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background: 
+                    'url(https://cdn.worldvectorlogo.com/logos/react.svg) center / cover'}}>React Projects</CardTitle>
+                    <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button> 
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            )
+        }else if(this.state.activeTab === 1){
+            return(
+                <div><h1>This is Angular</h1></div>
+            )
+        }else if(this.state.activeTab === 2){
+            return(
+                <div><h1>This is VuesJS</h1></div>
+            )
+        }else if(this.state.activeTab ===3){
+            return (
+                <div><h1>This is MongoDB</h1></div>
+            )
+        }
+
+    }
     render() {
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Angular</Tab>
+                    <Tab>VueJS</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+                </section>
+
+            </div>
         )
     }
 }
